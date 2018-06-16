e9f242ff0e932c4399bff4adff0c073a27ae7b16
Oleg Polyakov <olegpolyako@outlook.com>
2018-06-16 22:57:58 +0300

    Remove text icon

--- src/List/ListItemGraphic.jsx
+++ src/List/ListItemGraphic.jsx
@@ -2,23 +2,12 @@ import React from 'react';
 import classnames from 'classnames';
 
 export default function ListItemGraphic({
-    element = 'i',
-    component = element,
-    icon,
     className,
     children,
     ...props
 }) {
-    if (typeof icon === 'string') {
-        return React.createElement(component, {
-            className: classnames('mdc-list-item__graphic', {
-                'material-icons': icon && typeof icon === 'string',
-            }, className),
-            ...props
-        }, icon);
-    } else {
-        return React.cloneElement(icon || children, {
-            className: 'mdc-list-item__graphic'
-        });
-    }
+    return React.cloneElement(React.Children.only(children), {
+        className: classnames('mdc-list-item__graphic', className),
+        ...props
+    }, children);
 }
