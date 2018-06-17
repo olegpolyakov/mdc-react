0769060892df0a4bc5ad43d62b908e1e2e0fd74a
Oleg Polyakov <olegpolyako@outlook.com>
2018-06-17 19:06:13 +0300

    Add square and wide classes to CardMedia

--- src/Card/CardMedia.jsx
+++ src/Card/CardMedia.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import classnames from 'classnames';
 
-export default function CardMedia({ element = 'section', image, className, children, ...props }) {
-    
+export default function CardMedia({ element = 'section', image, square, wide, className, children, ...props }) {
     return React.createElement(element,
         {
-            className: classnames('mdc-card__media', 'mdc-card__media--16-9', className),
+            className: classnames('mdc-card__media', {
+                'mdc-card__media--square': square,
+                'mdc-card__media--16-9': wide,
+            }, className),
             style: image && {
                 backgroundImage: `url(${image})`
             },
             ...props
         },
-        children
+        children && React.createElement('div', { className: 'mdc-card__media-content' }, children)
     );
 }
