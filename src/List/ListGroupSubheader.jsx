9e4b2c90a7ec12199d017c11a2b12107a8d2a5f5
Oleg Polyakov <olegpolyako@outlook.com>
2018-06-17 19:04:44 +0300

    Finish ListGroupSubheader

--- src/List/ListGroupSubheader.jsx
+++ src/List/ListGroupSubheader.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import classnames from 'classnames';
 
-export default function ListGroupSubheader({ element = 'h3', title, children }) {
-    return React.createElement('h3', {
-        className: 'mdc-list-group__subheader'
+export default function ListGroupSubheader({ element = 'h3', title, className, children, ...props }) {
+    return React.createElement(element, {
+        className: classnames('mdc-list-group__subheader', className),
+        ...props
     }, title || children);
 }
