bd8ac2e1df2a9c6e57fc86766865039dfbe138fc
Oleg Polyakov <olegpolyako@outlook.com>
2018-06-17 19:04:59 +0300

    Finish ListGroup

--- src/List/ListGroup.jsx
+++ src/List/ListGroup.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import classnames from 'classnames';
 
-export default function ListGroup({ children }) {
-    return (
-        <div className="mdc-list-group">
-            {children}
-        </div>
-    );
+export default function ListGroup({ element = 'div', className, children, ...props }) {
+    return React.createElement(element, {
+        className: classnames('mdc-list-group', className),
+        ...props
+    }, children);
 }
