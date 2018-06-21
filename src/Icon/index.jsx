4c6c0574e97f420c2bb529a35c0ad85e02647d72
Oleg Polyakov <olegpolyako@outlook.com>
2018-06-21 15:57:03 +0300

    Update Icon

--- src/Icon/index.jsx
+++ src/Icon/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import classnames from 'classnames';
 
-export default function Icon({ element = 'i', className, ...props }) {
+export default function Icon({
+    element = 'i',
+    component = element,
+    size = 18,
+    dark,
+    light,
+    inactive,
+    className,
+    ...props
+}) {
     return (
         React.createElement(element, {
-            className: classnames('material-icons', className),
+            className: classnames('material-icons', {
+                [`md-${size}`]: size,
+                'md-dark': dark,
+                'md-light': light,
+                'md-inactive': inactive
+            }, className),
             ...props
         })
     );
