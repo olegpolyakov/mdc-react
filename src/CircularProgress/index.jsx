cbc0ed11b6167707ef0fa58240413b14e9ab41f7
Oleg Polyakov <olegpolyako@outlook.com>
2018-07-02 17:39:54 +0300

    Add prop classes

--- src/CircularProgress/index.jsx
+++ src/CircularProgress/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import classnames from 'classnames';
 
-export default function CircularProgress({ progress }) {
+export default function CircularProgress({ progress, className, ...props }) {
     const angle = progress * 289.067 / 100;
     const diff = 289.067 - angle;
 
     return (
-        <svg className="mdc-circular-progress" width="100%" height="100%" viewBox="0 0 100 100">
+        <svg className={classnames('mdc-circular-progress', className)} width="100%" height="100%" viewBox="0 0 100 100">
             <path
                 className="mdc-circular-progress__background"
                 fill="none"
                 strokeWidth="8"
                 d="M50,4 a46,46 0 0,1 0,92 a46,46 0 0,1 0,-92"
             />
+
             <path
                 className="mdc-circular-progress__foreground"
                 fill="none"
