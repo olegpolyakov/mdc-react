9323078fe15bd4dce0f57c2a808d1a99bbdf7428
Oleg Polyakov <olegpolyako@outlook.com>
2018-07-12 15:22:44 +0300

    Add HelperText

---
+++ src/TextField/HelperText.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import classnames from 'classnames';
+
+export default function HelperText({ element = 'p', persistent, error, children, ...props }) {
+    return React.createElement(element, {
+        className: classnames('mdc-text-field-helper-text', {
+            'mdc-text-field-helper-text--persistent': persistent,
+            'mdc-text-field-helper-text--validation-msg': error
+        })
+    }, children);
+};
