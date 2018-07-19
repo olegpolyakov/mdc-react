0535486c65e95afd1b37fcc48b67f796e4574c0b
Oleg Polyakov <olegpolyako@outlook.com>
2018-07-19 23:35:37 +0300

    Fix wrong prop

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import classnames from 'classnames';
 
-export default function Card({ element = 'div', outlined, className, children, ...props }) {
-    return React.createElement(element,
-        {
-            className: classnames('mdc-card', className, {
-                'mdc-card--outlined': outlined
-            }),
-            ...props
-        },
-        children
-    );
+export default function Card({
+    element = 'div',
+    component = element,
+    outlined,
+    className,
+    children,
+    ...props
+}) {
+    return React.createElement(component, {
+        className: classnames('mdc-card', className, {
+            'mdc-card--outlined': outlined
+        }),
+        ...props
+    }, children);
 }
