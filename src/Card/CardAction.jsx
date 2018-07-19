fe6055d1c2a16d01ccf7fc5d7253190275aa4dde
Oleg Polyakov <olegpolyako@outlook.com>
2018-07-19 23:35:56 +0300

    Fix props

--- src/Card/CardAction.jsx
+++ src/Card/CardAction.jsx
@@ -6,9 +6,9 @@ export default function CardAction({ component, children, button, icon, ...props
         React.cloneElement(component || React.Children.only(children), {
             className: classnames('mdc-card__action', {
                 'mdc-card__action--button': button,
-                'mdc-card__action--icon': icon,
-                ...props
-            })
+                'mdc-card__action--icon': icon
+            }),
+            ...props
         })
     );
 };
