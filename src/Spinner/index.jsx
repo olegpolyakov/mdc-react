1e9dace977eacaf06da21f8c18ed4d705a1eb910
Oleg Polyakov <olegpolyako@outlook.com>
2019-02-06 16:09:05 +0300

    Update Spinner component

--- src/Spinner/index.jsx
+++ src/Spinner/index.jsx
@@ -6,66 +6,18 @@ Spinner.displayName = 'MDCSpinner';
 import './index.scss';
 
 export default function Spinner({
-    active = true,
-    singleColor = true,
+    colorful = false,
 
     className,
     ...props
 }) {
     const classNames = classnames('mdc-spinner', {
-        'mdc-spinner--active': active,
-        'mdc-spinner--single-color': singleColor
+        'mdc-spinner--colorful': colorful
     }, className);
 
     return (
-        <div className={classNames} {...props}>
-            <div className="mdc-spinner__layer mdc-spinner__layer-1">
-                <div className="mdc-spinner__circle-clipper mdc-spinner__left">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-                <div className="mdc-spinner__gap-patch">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-                <div className="mdc-spinner__circle-clipper mdc-spinner__right">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-            </div>
-
-            <div className="mdc-spinner__layer mdc-spinner__layer-2">
-                <div className="mdc-spinner__circle-clipper mdc-spinner__left">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-                <div className="mdc-spinner__gap-patch">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-                <div className="mdc-spinner__circle-clipper mdc-spinner__right">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-            </div>
-
-            <div className="mdc-spinner__layer mdc-spinner__layer-3">
-                <div className="mdc-spinner__circle-clipper mdc-spinner__left">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-                <div className="mdc-spinner__gap-patch">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-                <div className="mdc-spinner__circle-clipper mdc-spinner__right">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-            </div>
-
-            <div className="mdc-spinner__layer mdc-spinner__layer-4">
-                <div className="mdc-spinner__circle-clipper mdc-spinner__left">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-                <div className="mdc-spinner__gap-patch">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-                <div className="mdc-spinner__circle-clipper mdc-spinner__right">
-                    <div className="mdc-spinner__circle"></div>
-                </div>
-            </div>
-        </div>
+        <svg className={classNames} viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <circle className="mdc-spinner__circle" cx="33" cy="33" r="30" />
+        </svg>
     );
 }
