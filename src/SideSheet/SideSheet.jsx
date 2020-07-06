112063bfe1fd82373f077457381805b4a980eeb4
Oleg Polyakov <olegpolyakov@outlook.com>
2020-07-06 18:44:11 +0300

    Update SideSheet

--- src/SideSheet/SideSheet.jsx
+++ src/SideSheet/SideSheet.jsx
@@ -1,29 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransition } from 'react-transition-group';
 import classnames from 'classnames';
 
 import { useMounted, useUpdated } from '../lifecycle-hooks';
-
-const cssClasses = {
-    ROOT: 'mdc-side-sheet',
-    DISMISSIBLE: 'mdc-side-sheet--dismissible',
-    MODAL: 'mdc-side-sheet--modal',
-    OPEN: 'mdc-side-sheet--open',
-    ANIMATE: 'mdc-side-sheet--animate',
-    OPENING: 'mdc-side-sheet--opening',
-    CLOSING: 'mdc-side-sheet--closing',
-    CONTENT: 'mdc-side-sheet__content',
-    APP_CONTENT: 'mdc-side-sheet-app-content',
-    SCRIM: 'mdc-side-sheet-scrim'
-};
+import Modal from '../Modal';
 
 export default function SideSheet({
+    title,
     open = false,
     appear = false,
     dismissible = false,
     modal = false,
-    appContentSelector,
     onClose = Function.prototype,
 
     element = 'aside',
@@ -32,17 +20,11 @@ export default function SideSheet({
     children,
     ...props
 }) {
-    const rootElement = React.useRef();
-    const scrimElement = React.useRef();
-
-    const classNames = classnames(cssClasses.ROOT, {
-        [cssClasses.DISMISSIBLE]: dismissible,
-        [cssClasses.MODAL]: modal
-    }, className);
+    const rootElement = useRef();
 
     useMounted(() => {
-        if (dismissible && appContentSelector) {
-            document.querySelector(appContentSelector).classList.add(cssClasses.APP_CONTENT);
+        if (dismissible) {
+            rootElement.current.nextElementSibling.classList.add(cssClasses.APP_CONTENT);
         }
     });
 
@@ -60,7 +42,12 @@ export default function SideSheet({
                 document.removeEventListener('keydown', handleDocumentKeyDown);
             }
         }
-    }, [open]);
+    }, [open, modal]);
+
+    const classNames = classnames(cssClasses.ROOT, {
+        [cssClasses.DISMISSIBLE]: dismissible,
+        [cssClasses.MODAL]: modal
+    }, className);
 
     return (
         <CSSTransition
@@ -71,44 +58,82 @@ export default function SideSheet({
                 exit: 200
             }}
             classNames={{
-                appear: `${cssClasses.OPEN}`,
+                appear: cssClasses.OPEN,
                 enter: `${cssClasses.OPEN} ${cssClasses.ANIMATE}`,
                 enterActive: `${cssClasses.OPEN} ${cssClasses.OPENING}`,
                 enterDone: cssClasses.OPEN,
                 exit: `${cssClasses.OPEN} ${cssClasses.CLOSING}`,
-                exitActive: `${cssClasses.CLOSING}`
+                exitActive: cssClasses.CLOSING
             }}
+            mountOnEnter={modal}
+            unmountOnExit={modal}
         >
-            <React.Fragment>
-                <Element
-                    className={classNames}
-                    ref={rootElement}
-                    {...props}
-                >
-                    <div className={cssClasses.CONTENT}>
-                        {children}
-                    </div>
-                </Element>
+            {modal ?
+                <Modal fixed>
+                    <Element
+                        ref={rootElement}
+                        className={classNames}
+                        {...props}
+                    >
+                        {title &&
+                            <header className="mdc-side-sheet__header">
+                                <h3 className="mdc-side-sheet__title">{title}</h3>
+                                <i className="mdc-icon mdc-icon-button material-icons mdc-side-sheet__close-button" onClick={onClose}>close</i>
+                            </header>
+                        }
 
-                {modal &&
+                        <section className="mdc-side-sheet__content">
+                            {children}
+                        </section>
+                    </Element>
+
                     <div
                         className={cssClasses.SCRIM}
-                        ref={scrimElement}
                         onClick={onClose}
                     />
-                }
-            </React.Fragment>
+                </Modal>
+                :
+                <Element
+                    ref={rootElement}
+                    className={classNames}
+                    {...props}
+                >
+                    {title &&
+                        <header className="mdc-side-sheet__header">
+                            <h3 className="mdc-side-sheet__title">{title}</h3>
+                            <i className="mdc-icon mdc-icon-button material-icons mdc-side-sheet__close-button" onClick={onClose}>close</i>
+                        </header>
+                    }
+
+                    <section className="mdc-side-sheet__content">
+                        {children}
+                    </section>
+                </Element>
+            }
         </CSSTransition>
     );
 }
 
+const cssClasses = {
+    ROOT: 'mdc-side-sheet',
+    CONTENT: 'mdc-side-sheet__content',
+    DISMISSIBLE: 'mdc-side-sheet--dismissible',
+    MODAL: 'mdc-side-sheet--modal',
+    OPEN: 'mdc-side-sheet--open',
+    ANIMATE: 'mdc-side-sheet--animate',
+    OPENING: 'mdc-side-sheet--opening',
+    CLOSING: 'mdc-side-sheet--closing',
+    APP_CONTENT: 'mdc-side-sheet-app-content',
+    SCRIM: 'mdc-side-sheet-scrim',
+};
+
 SideSheet.displayName = 'MDCSideSheet';
 
 SideSheet.propTypes = {
+    title: PropTypes.string,
     open: PropTypes.bool,
     appear: PropTypes.bool,
     dismissible: PropTypes.bool,
     modal: PropTypes.bool,
-    appContentSelector: PropTypes.string,
     onClose: PropTypes.func
 };
