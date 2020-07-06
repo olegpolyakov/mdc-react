c52a6169884feade78f04ec1afb147c8236233d1
Oleg Polyakov <olegpolyakov@outlook.com>
2020-07-06 18:31:19 +0300

    Update Radio

--- src/Radio/Radio.jsx
+++ src/Radio/Radio.jsx
@@ -1,35 +1,31 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-
-import { useRendered } from '../lifecycle-hooks';
 
 export default function Radio({
     value,
     checked = false,
     disabled = false,
     onChange = Function.prototype,
-    
+
     className,
     ...props
 }) {
-    const input = React.useRef();
+    const inputRef = useRef();
 
-    const handleChange = event => onChange(value, input.current, event);
+    const handleChange = useCallback(event => {
+        onChange(event, event.target.value, event.target);
+    }, []);
 
     const classNames = classnames('mdc-radio', {
         'mdc-radio--disabled': disabled
     }, className);
 
-    useRendered(() => {
-        input.current.blur();
-    }, [checked]);
-
     return (
         <div className={classNames}>
             <input
+                ref={inputRef}
                 className="mdc-radio__native-control"
-                ref={input}
                 type="radio"
                 value={value}
                 checked={checked}
@@ -43,7 +39,7 @@ export default function Radio({
                 <div className="mdc-radio__inner-circle" />
             </div>
 
-            <div className="mdc-radio__ripple"></div>
+            <div className="mdc-radio__ripple" />
         </div>
     );
 }
@@ -51,6 +47,7 @@ export default function Radio({
 Radio.displayName = 'MDCRadio';
 
 Radio.propTypes = {
+    value: PropTypes.string.isRequired,
     checked: PropTypes.bool.isRequired,
     disabled: PropTypes.bool,
     onChange: PropTypes.func.isRequired
