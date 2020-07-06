c5401cb826bfd84ab53aab9cf246f08dc5fe4f92
Oleg Polyakov <olegpolyakov@outlook.com>
2020-07-06 18:31:32 +0300

    Add DropdownIcon

---
+++ src/Select/DropdownIcon.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function DropdownIcon() {
+    return (
+        <span className="mdc-select__dropdown-icon">
+            <svg
+                className="mdc-select__dropdown-icon-graphic"
+                viewBox="7 10 10 5"
+            >
+                <polygon
+                    className="mdc-select__dropdown-icon-inactive"
+                    stroke="none"
+                    fillRule="evenodd"
+                    points="7 10 12 15 17 10"
+                />
+
+                <polygon
+                    className="mdc-select__dropdown-icon-active"
+                    stroke="none"
+                    fillRule="evenodd"
+                    points="7 15 12 10 17 15"
+                />
+            </svg>
+        </span>
+    );
+}
