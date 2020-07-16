021d50446f831c83767f6a8bd456907e6c8e6084
Oleg Polyakov <olegpolyakov@outlook.com>
2020-07-16 17:23:08 +0300

    ListDivider: Update inset prop

--- src/List/ListDivider.jsx
+++ src/List/ListDivider.jsx
@@ -3,19 +3,14 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 export default function ListDivider({
-    inset = false,
-    padded = false,
+    inset,
 
     element: Element = 'li',
     className,
     ...props
 }) {
     const classNames = classnames('mdc-list-divider', {
-        'mdc-list-divider--padded': padded,
-        'mdc-list-divider--inset': inset === true,
-        'mdc-list-divider--inset-leading': inset === 'leading',
-        'mdc-list-divider--inset-trailing': inset === 'trailing',
-        'mdc-list-divider--inset-padding': inset === 'padding'
+        [`mdc-list-divider--inset-${inset}`]: inset
     }, className);
 
     return (
@@ -30,9 +25,5 @@ export default function ListDivider({
 ListDivider.displayName = 'MDCListDivider';
 
 ListDivider.propTypes = {
-    padded: PropTypes.bool,
-    inset: PropTypes.oneOfType([
-        PropTypes.bool,
-        PropTypes.string
-    ])
+    inset: PropTypes.oneOf(['leading', 'trailing', 'padding'])
 };
