79fe83efac457117a32131ce2dc434c4159d8665
Oleg Polyakov <olegpolyakov@outlook.com>
2020-07-16 17:23:22 +0300

    ListGroup: Add subheader prop

--- src/List/ListGroup.jsx
+++ src/List/ListGroup.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import ListGroupSubheader from './ListGroupSubheader';
+
 export default function ListGroup({
+    subheader,
+
     element: Element = 'div',
     className,
+    children,
     ...props
 }) {
     const classNames = classnames('mdc-list-group', className);
 
     return (
-        <Element className={classNames} {...props} />
+        <Element className={classNames} {...props}>
+            {subheader &&
+                <ListGroupSubheader>{subheader}</ListGroupSubheader>
+            }
+
+            {children}
+        </Element>
     );
 }
 
 ListGroup.displayName = 'MDCListGroup';
+
+ListGroup.propTypes = {
+    subheader: PropTypes.node
+};
+
+ListGroup.Subheader = ListGroupSubheader;
