4e49e0b7b6d8e2ff264dd83519e7d48ce05d6bb4
Oleg Polyakov <olegpolyakov@outlook.com>
2020-07-27 10:07:25 +0300

    Menu: Add control with keyboard

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -1,32 +1,62 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import { useEffect, useUpdated } from '../lifecycle-hooks';
 import MenuSurface from '../MenuSurface/MenuSurface';
 import List from '../List/List';
 
-export default function Menu({
+export default React.forwardRef(Menu);
+
+function Menu({
     open,
     listProps = {},
 
     children,
     className,
     ...props
-}) {
+}, ref) {
     const listRef = useRef();
-    const classNames = classnames('mdc-menu', className);
-    const hasSelectedChildren = React.Children.toArray(children).some(i => i.props.selected);
+    const [focusedChildIndex, setFocusedChildIndex] = useState(0);
 
     useEffect(() => {
         if (open) {
-            listRef.current?.focus();
+            setFocusedChildIndex(
+                React.Children.toArray(children).findIndex(i => i.props.selected)
+            );
         }
     }, [open]);
 
+    useUpdated(() => {
+        listRef.current.children[focusedChildIndex]?.focus();
+    }, [focusedChildIndex]);
+
+    const handleKeyDown = useCallback(event => {
+        event.preventDefault();
+
+        if (event.key === 'ArrowDown') {
+            setFocusedChildIndex(index => {
+                const nextIndex = index + 1;
+
+                return nextIndex < listRef.current.children.length ? nextIndex : index;
+            });
+        } else if (event.key === 'ArrowUp') {
+            setFocusedChildIndex(index => {
+                const nextIndex = index - 1;
+
+                return nextIndex >= 0 ? nextIndex : index;
+            });
+        }
+    }, []);
+
+    const classNames = classnames('mdc-menu', className);
+
     return (
         <MenuSurface
+            ref={ref}
             open={open}
             className={classNames}
+            onKeyDown={handleKeyDown}
             {...props}
         >
             <List
@@ -39,9 +69,7 @@ export default function Menu({
             >
                 {React.Children.map(children, (child, index) =>
                     React.cloneElement(child, {
-                        tabIndex: hasSelectedChildren ?
-                            (child.props.selected ? 0 : -1) :
-                            (index === 0) ? 0 : -1
+                        tabIndex: (index === focusedChildIndex) ? 0 : -1
                     })
                 )}
             </List>
