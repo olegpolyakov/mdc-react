5a3d50976f0e7fbb780428bd44a9a3f42339f7a4
Oleg Polyakov <olegpolyakov@outlook.com>
2020-07-31 15:07:30 +0300

    SelectOption: Add checkbox

--- src/Select/SelectOption.jsx
+++ src/Select/SelectOption.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
+import Checkbox from '../Checkbox/Checkbox';
+import ListItem from '../List/ListItem';
 import MenuItem from '../Menu/MenuItem';
 
 export default React.forwardRef(SelectOption);
 
-function SelectOption(props) {
-    return <MenuItem {...props} />;
+function SelectOption({
+    selected,
+    checkbox = false,
+    onClick,
+
+    children,
+    ...props
+}, ref) {
+    return (
+        <MenuItem
+            ref={ref}
+            selected={selected}
+            onClick={onClick}
+            {...props}
+        >
+            {checkbox &&
+                <ListItem.Graphic>
+                    <Checkbox checked={selected} />
+                </ListItem.Graphic>
+            }
+
+            {children}
+        </MenuItem>
+    );
 }
 
 SelectOption.displayName = 'MDCSelectOption';
