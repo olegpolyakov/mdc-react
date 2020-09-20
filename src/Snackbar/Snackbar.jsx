7f9e4beb09c3cf17758ef89633f18936e8a03f40
Oleg Polyakov <olegpolyakov@outlook.com>
2020-09-20 22:14:39 +0300

    Snackbar: Add clearTimeout on unmount

--- src/Snackbar/Snackbar.jsx
+++ src/Snackbar/Snackbar.jsx
@@ -39,6 +39,8 @@ function Snackbar({
                 timeoutRef.current = null;
             }
         }
+
+        return () => clearTimeout(timeoutRef.current);
     }, [open]);
 
     const handleKeyDown = useCallback(event => {
