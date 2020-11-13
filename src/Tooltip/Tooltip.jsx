0050e361e77db17b9b53a8c821c811f7afde1e53
Oleg Polyakov <olegpolyakov@outlook.com>
2020-11-13 17:50:20 +0300

    Update tooltip

--- src/Tooltip/Tooltip.jsx
+++ src/Tooltip/Tooltip.jsx
@@ -1,14 +1,19 @@
 import React, { useRef, useState, useCallback, useImperativeHandle } from 'react';
 import classnames from 'classnames';
 
-import { useUpdated } from '../lifecycle-hooks';
+import { useMounted } from '../lifecycle-hooks';
 import Layer from '../Layer';
 
 export default React.forwardRef(Tooltip);
 
+const MIN_HEIGHT = 24;
+const MAX_WIDTH = 200;
+const MIN_VIEWPORT_TOOLTIP_THRESHOLD = 32;
+const BOUNDED_ANCHOR_GAP = 4;
+const UNBOUNDED_ANCHOR_GAP = 8;
+
 function Tooltip({
     label,
-    large = false,
 
     element: Element = 'div',
     className,
@@ -20,13 +25,14 @@ function Tooltip({
 
     useImperativeHandle(ref, () => tooltipRef.current);
 
-    useUpdated(() => {
-        const targetRect = targetRef.current?.getBoundingClientRect();
-
-        if (targetRect) {
-            tooltipRef.current.style.top = (window.scrollY + targetRect.top + targetRect.height + 8) + 'px';
-            tooltipRef.current.style.left = (window.scrollX + targetRect.left + targetRect.width * 0.5) + 'px';
+    useMounted(() => {
+        function handleScroll() {
+            setActive(active => active && !active);
         }
+
+        document.addEventListener('scroll', handleScroll);
+
+        return () => document.removeEventListener('scroll', handleScroll);
     }, [active]);
 
     const handleMouseEnter = useCallback(event => {
@@ -39,9 +45,35 @@ function Tooltip({
         setActive(false);
     }, []);
 
-    const classNames = classnames('mdc-tooltip', {
-        'mdc-tooltip--large': large
-    }, className);
+    const handleEntering = useCallback(() => {
+        const tooltipRect = tooltipRef.current.getBoundingClientRect();
+        const targetRect = targetRef.current.getBoundingClientRect();
+
+        if (tooltipRect.height > MIN_HEIGHT && tooltipRect.width >= MAX_WIDTH) {
+            tooltipRef.current.classList.add('mdc-tooltip--multiline');
+        }
+
+        const top = (targetRect.top + targetRect.height + UNBOUNDED_ANCHOR_GAP);
+        const left = (targetRect.left + targetRect.width * 0.5 - tooltipRect.width * 0.5);
+        const right = left + tooltipRect.width;
+        const bottom = top + tooltipRect.height;
+        const deltaBottom = window.innerHeight - bottom;
+        const deltaRight = window.innerWidth - right;
+
+        if (deltaBottom < 0) {
+            tooltipRef.current.style.top = `${targetRect.top - tooltipRect.height - UNBOUNDED_ANCHOR_GAP}px`;
+        } else {
+            tooltipRef.current.style.top = `${top}px`;
+        }
+
+        if (deltaRight < 0) {
+            tooltipRef.current.style.left = `${targetRect.right - tooltipRect.width}px`;
+        } else {
+            tooltipRef.current.style.left = `${left < 0 ? targetRect.left : left}px`;
+        }
+    }, []);
+
+    const classNames = classnames('mdc-tooltip', className);
 
     return (
         <>
@@ -53,15 +85,23 @@ function Tooltip({
             <Layer
                 modal
                 in={active}
-                timeout={{ enter: 150, exit: 0 }}
+                timeout={{ enter: 500, exit: 100 }}
                 classNames={{
-                    enterDone: 'mdc-tooltip--active'
+                    enter: 'mdc-tooltip--showing-transition',
+                    enterActive: 'mdc-tooltip--showing-transition mdc-tooltip--showing',
+                    enterDone: 'mdc-tooltip--showing-transition mdc-tooltip--shown',
+                    exit: 'mdc-tooltip--hide-transition mdc-tooltip--hide',
+                    exitActive: 'mdc-tooltip--hide-transition mdc-tooltip--hide',
+                    exitDone: ''
                 }}
+                onEntering={handleEntering}
                 mountOnEnter
                 unmountOnExit
             >
                 <Element ref={tooltipRef} className={classNames}>
-                    {label}
+                    <div className="mdc-tooltip__surface">
+                        {label}
+                    </div>
                 </Element>
             </Layer>
         </>
