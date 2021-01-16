f49543e3452a31404dfafd4077eb6c51c30d188f
philipp <philipp.sapronov@gmail.com>
2021-01-16 22:18:09 +0200

    Modal: Add types

---
+++ src/Modal/Modal.d.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+import {HTMLElementTagName} from '../types';
+
+type ModalProps = {
+    fixed?: boolean;
+    children: React.ReactNode;
+    element: HTMLElementTagName;
+};
+
+// TODO: check ReactDOM createPortal ReturnType
+export default function (props: ModalProps): JSX.Element;
