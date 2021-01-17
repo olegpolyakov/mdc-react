0d2ef54aa8ccb66f81378bc0b6170b3b6a445448
philipp <philipp.sapronov@gmail.com>
2021-01-17 14:58:08 +0200

    Button: Refactoring of types

--- src/Button/Button.d.ts
+++ src/Button/Button.d.ts
@@ -1,19 +1,27 @@
-import {HTMLElementMap, HTMLElementTagName, InferredComponent, RefForwardingProps} from '../types';
+import {
+    HTMLElementMap,
+    HTMLElementTagName,
+    InferredComponent,
+    PropsWithElementAndComponent,
+    RefForwardingProps
+} from '../types';
 import React from 'react';
 
-export type ButtonProps = {
+type Props = {
     label: string;
-    icon: React.ReactElement;
-    leadingIcon: React.ReactElement;
-    trailingIcon: React.ReactElement;
+    icon: React.ReactNode;
+    leadingIcon: React.ReactNode;
+    trailingIcon: React.ReactNode;
     raised?: boolean;
     unelevated?: boolean;
     outlined?: boolean;
     className?: string;
 };
 
+export type ButtonProps<E, C, R> = PropsWithElementAndComponent<E, C, R> & Props;
+
 export default function <
-    TName extends HTMLElementTagName = 'span',
-    TRef extends HTMLElementMap<TName>,
-    TComponent extends InferredComponent<TComponent>
->(props: ButtonProps & RefForwardingProps<TName, TRef, TComponent>): JSX.Element;
+    TElement extends HTMLElementTagName = 'button',
+    TComponent extends InferredComponent<TComponent>,
+    TRef extends HTMLElementMap<TElement>
+>(props: ButtonProps & RefForwardingProps<TElement, TComponent, TRef>): JSX.Element;
