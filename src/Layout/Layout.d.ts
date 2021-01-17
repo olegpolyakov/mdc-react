27c4afb8f003bbb494cb70a21d77046f6bba8188
philipp <philipp.sapronov@gmail.com>
2021-01-18 01:57:51 +0200

    Layout: Refactoring of types -> remove useless generic

--- src/Layout/Layout.d.ts
+++ src/Layout/Layout.d.ts
@@ -1,4 +1,4 @@
-import {HTMLElementMap, HTMLElementTagName, InferredComponent, PropsWithElementAndComponent} from '../types';
+import { HTMLElementTagName, InferredComponent, PropsWithElementAndComponent } from '../types';
 
 type JustifyContent = 'start' | 'center' | 'end' | 'around' | 'between';
 type AlignSelf = 'start' | 'center' | 'end';
@@ -15,10 +15,9 @@ type Props = {
     className?: string;
 };
 
-export type LayoutProps<E, C, R> = PropsWithElementAndComponent<E, C, R> & Props;
+export type LayoutProps<E = any, C = any> = PropsWithElementAndComponent<E, C> & Props;
 
 export default function <
     TElement extends HTMLElementTagName = 'span',
-    TComponent extends InferredComponent<TComponent>,
-    TRef extends HTMLElementMap<TElement>
->(props: LayoutProps<TElement, TComponent, TRef>): JSX.Element;
+    TComponent extends InferredComponent<TComponent>
+>(props: LayoutProps<TElement, TComponent>): JSX.Element;
