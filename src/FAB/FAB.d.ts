050908627fd3a36ac760e92397639f314bc0e67b
philipp <philipp.sapronov@gmail.com>
2021-01-18 01:53:56 +0200

    FAB: Refactoring of types -> remove useless generic

--- src/FAB/FAB.d.ts
+++ src/FAB/FAB.d.ts
@@ -1,4 +1,4 @@
-import {HTMLElementMap, HTMLElementTagName, InferredComponent, PropsWithElementAndComponent} from '../types';
+import { HTMLElementTagName, InferredComponent, PropsWithElementAndComponent } from '../types';
 import React from 'react';
 
 type Props = {
@@ -12,10 +12,9 @@ type Props = {
     className: string;
 };
 
-export type FABProps<E, C, R> = PropsWithElementAndComponent<E, C, R> & Props;
+export type FABProps<E = any, C = any> = PropsWithElementAndComponent<E, C> & Props;
 
 export default function <
     TElement extends HTMLElementTagName = 'span',
-    TComponent extends InferredComponent<TComponent>,
-    TRef extends HTMLElementMap<TElement>
->(props: FABProps<TElement, TComponent, TRef>): JSX.Element;
+    TComponent extends InferredComponent<TComponent>
+>(props: FABProps<TElement, TComponent>): JSX.Element;
