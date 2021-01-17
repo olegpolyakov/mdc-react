09866e713210b20d0949e26b72140acd81c32ada
philipp <philipp.sapronov@gmail.com>
2021-01-17 15:06:44 +0200

    Tooltip: Refactoring of types

--- src/Tooltip/Tooltip.d.ts
+++ src/Tooltip/Tooltip.d.ts
@@ -1,11 +1,16 @@
-import React, {PropsWithChildren} from 'react';
-import {HTMLElementTagName} from '../types';
+import React from 'react';
+import {HTMLElementMap, HTMLElementTagName, PropsWithElement} from '../types';
 
-type TooltipProps = PropsWithChildren<{
+type Props = {
     label?: React.ReactNode;
     element?: HTMLElementTagName;
     className?: string;
     ref: HTMLElement;
-}>;
+};
 
-export default function (props: TooltipProps): JSX.Element;
+// useImperativeHandle
+export type TooltipProps<E, R> = PropsWithElement<E, R> & Props;
+
+export default function <TElement extends HTMLElementTagName = 'span', TRef extends HTMLElementMap<TElement>>(
+    props: TooltipProps<TElement, TRef>
+): JSX.Element;
