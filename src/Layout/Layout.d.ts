767dbe6c4e00340477bc5f9e964d8f5cdfb11654
philipp <philipp.sapronov@gmail.com>
2021-01-17 15:02:01 +0200

    Layout: Refactoring of types

--- src/Layout/Layout.d.ts
+++ src/Layout/Layout.d.ts
@@ -1,22 +1,24 @@
-import {HTMLElementMap, HTMLElementTagName, InferredComponent, RefForwardingProps} from '../types';
+import {HTMLElementMap, HTMLElementTagName, InferredComponent, PropsWithElementAndComponent} from '../types';
 
 type JustifyContent = 'start' | 'center' | 'end' | 'around' | 'between';
 type AlignSelf = 'start' | 'center' | 'end';
 type AlignItems = 'start' | 'center' | 'end';
 type Direction = 'row' | 'column';
 
-type LayoutProps = {
+type Props = {
     row?: boolean;
     column?: boolean;
     direction?: Direction;
     alignItems?: AlignItems;
     alignSelf?: AlignSelf;
-    justifyContent: JustifyContent;
+    justifyContent?: JustifyContent;
     className?: string;
 };
 
+export type LayoutProps<E, C, R> = PropsWithElementAndComponent<E, C, R> & Props;
+
 export default function <
-    TName extends HTMLElementTagName = 'span',
-    TRef extends HTMLElementMap<TName>,
-    TComponent extends InferredComponent<TComponent>
->(props: LayoutProps & RefForwardingProps<TName, TRef, TComponent>): JSX.Element;
+    TElement extends HTMLElementTagName = 'span',
+    TComponent extends InferredComponent<TComponent>,
+    TRef extends HTMLElementMap<TElement>
+>(props: LayoutProps<TElement, TComponent, TRef>): JSX.Element;
