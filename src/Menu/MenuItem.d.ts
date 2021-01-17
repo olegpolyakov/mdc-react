7256c2a3a4b123d00f9bf51a10a6f076d57ab3c2
philipp <philipp.sapronov@gmail.com>
2021-01-17 17:53:02 +0200

    Menu: Export Props types

--- src/Menu/MenuItem.d.ts
+++ src/Menu/MenuItem.d.ts
@@ -1,7 +1,7 @@
 import {HTMLElementMap, HTMLElementTagName} from '../types';
 import {ListItemProps} from '../List/ListItem';
 
-type MenuItemProps<E = any, R = any> = {
+export type MenuItemProps<E = any, R = any> = {
     selected?: boolean;
     disabled?: boolean;
 } & ListItemProps<E, R>;
